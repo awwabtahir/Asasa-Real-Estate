bb898cab002bfe084f1e8f5220ddadb599654375
Awwab Tahir <awwabtahir11@gmail.com>
2018-10-29 14:58:02 +0500

    liveagent updated - 29 Oct 2018

--- client/js/liveagent.js
+++ client/js/liveagent.js
@@ -1,3 +1,15 @@
+// For text
+(function (d, src, c) { var t = d.scripts[d.scripts.length - 1], s = d.createElement('script'); s.id = 'la_x2s6df8d'; s.async = true; s.src = src; s.onload = s.onreadystatechange = function () { var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) { return; } c(this); }; t.parentElement.insertBefore(s, t.nextSibling); })(document,
+    'https://asasa.ladesk.com/scripts/track.js',
+    function (e) { LiveAgent.createButton('446a48ae', e); });
+
+// For Call
+(function (d, src, c) { var t = d.scripts[d.scripts.length - 1], s = d.createElement('script'); s.id = 'la_x2s6df8d'; s.async = true; s.src = src; s.onload = s.onreadystatechange = function () { var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) { return; } c(this); }; t.parentElement.insertBefore(s, t.nextSibling); })(document,
+    'https://asasa.ladesk.com/scripts/track.js',
+    function (e) { LiveAgent.createButton('c1045226', e); });
+
+
+// For Feedback
 (function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.async=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
-'https://asasa.ladesk.com/scripts/track.js',
-function(e){ LiveAgent.createButton('446a48ae', e); });
+    'https://asasa.ladesk.com/scripts/track.js',
+    function(e){ LiveAgent.createButton('4bec48d5', e); });
