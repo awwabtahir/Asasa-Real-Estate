98229559936abb2c38b158d50ddf9d07213a3082
Awwab Tahir <awwabtahir11@gmail.com>
2018-10-30 13:13:18 +0500

    liveagent fixed for mobile - 30 Oct 2018

--- client/js/liveagent.js
+++ client/js/liveagent.js
@@ -1,7 +1,10 @@
 // For text
 (function (d, src, c) { var t = d.scripts[d.scripts.length - 1], s = d.createElement('script'); s.id = 'la_x2s6df8d'; s.async = true; s.src = src; s.onload = s.onreadystatechange = function () { var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) { return; } c(this); }; t.parentElement.insertBefore(s, t.nextSibling); })(document,
     'https://asasa.ladesk.com/scripts/track.js',
-    function (e) { LiveAgent.createButton('446a48ae', e); });
+    function (e) {
+        LiveAgent.instance.isMobileBrowser = false;
+        LiveAgent.createButton('446a48ae', e);
+    });
 
 // For Call
 (function (d, src, c) { var t = d.scripts[d.scripts.length - 1], s = d.createElement('script'); s.id = 'la_x2s6df8d'; s.async = true; s.src = src; s.onload = s.onreadystatechange = function () { var rs = this.readyState; if (rs && (rs != 'complete') && (rs != 'loaded')) { return; } c(this); }; t.parentElement.insertBefore(s, t.nextSibling); })(document,
