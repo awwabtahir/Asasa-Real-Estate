232ae8fef652202b4bc2b4bcf00a15d3fc6c4a0c
Awwab Tahir <awwabtahir11@gmail.com>
2019-01-02 12:47:38 +0500

    call modal added - 2 Jan 2019

--- client/src/app/home/call/call.component.ts
+++ client/src/app/home/call/call.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'call',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./call.component.css']
 })
 export class CallComponent implements OnInit {
+
+  @Input() id;
 
   constructor() { }
 
