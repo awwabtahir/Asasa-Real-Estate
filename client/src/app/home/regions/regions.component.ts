22c66f9c6725cdea17df7e82738bb3e074120b45
Awwab Tahir <awwabtahir11@gmail.com>
2019-02-25 12:48:35 +0500

    Adding properties count in hot regions

--- client/src/app/home/regions/regions.component.ts
+++ client/src/app/home/regions/regions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'app/authentication.service';
 
 @Component({
   selector: 'regions',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegionsComponent implements OnInit {
 
-  constructor() { }
+  cities;
+  locations;
 
-  ngOnInit() {
+  islamabadCount = 0;
+  peshawarCount = 0;
+  karachiCount = 0;
+
+  constructor(
+    private auth: AuthenticationService
+  ) { }
+
+  async ngOnInit() {
+    this.getCities();
+    this.getLocations();
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    this.islamabadCount = this.getPropCounts("Islamabad");
+    this.peshawarCount = this.getPropCounts("Peshawar");
+    this.karachiCount = this.getPropCounts("Karachi");
+  }
+
+  getPropCounts(cityName) {
+
+    let city = this.cities.filter(function (c) {
+      return c.city == cityName;
+    });
+    let cityId = city[0]._id;
+
+    let locations = this.locations.filter(function (i) {
+      return i.cityId == cityId;
+    });
+
+    return locations.length;
+  }
+
+  getCities() {
+    this.auth.getCities().subscribe(cities => {
+      this.cities = cities;
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+  getLocations(selectedCity?) {
+    this.auth.getLocations().subscribe(locations => {
+      this.locations = locations;
+    }, (err) => {
+      console.error(err);
+    });
   }
 
 }
