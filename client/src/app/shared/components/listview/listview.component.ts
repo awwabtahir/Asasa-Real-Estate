83d1a60952f868faf8f3455e6a52876cef6e6625
Usama Javaid <usamajavaid52@gmail.com>
2019-04-17 14:43:46 +0500

    filters implemented on list view

--- client/src/app/shared/components/listview/listview.component.ts
+++ client/src/app/shared/components/listview/listview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { PropertyService } from "shared/services/property.service";
 import { LocationService } from "shared/services/location.service";
+import { FilterService } from "shared/services/filter.service";
+import { MapService } from "shared/services/map.service";
 
 @Component({
   selector: "listview",
@@ -14,15 +16,23 @@ export class ListviewComponent implements OnInit {
   location: any;
   city: any;
   p: number = 1;
+  filteredViaLocation: any;
+
+  filterOpts;
+  filteredAds = [];
+  type;
 
   constructor(
     private propertyService: PropertyService,
-    private locationService: LocationService
+    private locationService: LocationService,
+    private filterService: FilterService,
+    private mapService: MapService
   ) {}
 
   ngOnInit() {
     this.getAds();
-    // console.log(this.locationService.cityObj, this.locationService.locObj);
+    this.filter();
+
     this.locationService.locChange.subscribe(val => {
       this.location = val;
       this.filteredByLocation();
@@ -41,14 +51,14 @@ export class ListviewComponent implements OnInit {
     return ctx;
   }
 
-  getAds() {
-    this.propertyService.getAds().subscribe(
+  async getAds() {
+    await this.propertyService.getAds().subscribe(
       properties => {
         this.properties = properties;
 
         this.propertiesAvailable = true;
         if (!this.list) {
-          this.getList();
+          this.getList(this.properties);
         }
       },
       err => {
@@ -57,10 +67,30 @@ export class ListviewComponent implements OnInit {
     );
   }
 
-  getList() {
-    let result = this.properties.map(a => a._id);
+  filter() {
+    this.filterOpts = this.filterService
+      .getFilterOpts()
+      .subscribe(filterOpts => {
+        this.applyFilter(filterOpts);
+      });
+    this.type = this.mapService.getType().subscribe(type => {
+      if (this.filteredViaLocation) {
+        this.filteredAds = this.filteredViaLocation;
+      } else this.filteredAds = this.properties;
+      this.filteredAds = this.filteredAds.filter(function(ad) {
+        return ad.subtype.toLowerCase() == type.toLowerCase();
+      });
+      this.getList(this.filteredAds);
+    });
+  }
+
+  getList(properties) {
+    let result = properties.map(a => a._id);
 
     this.list = result;
+    if (this.list.length == 0) {
+      this.propertiesAvailable = false;
+    }
   }
 
   filteredByCity() {
@@ -68,11 +98,8 @@ export class ListviewComponent implements OnInit {
     const result = byCityList.filter(
       prop => prop.locationData.city == this.city.city
     );
-    let filtered = result.map(a => a._id);
-    this.list = filtered;
-    if (this.list.length == 0) {
-      this.propertiesAvailable = false;
-    }
+    this.filteredViaLocation = result;
+    this.getList(result);
   }
 
   filteredByLocation() {
@@ -80,10 +107,23 @@ export class ListviewComponent implements OnInit {
     const result = byLocationList.filter(
       prop => prop.locationData.location == this.location.location
     );
-    let filtered = result.map(a => a._id);
-    this.list = filtered;
-    if (this.list.length == 0) {
-      this.propertiesAvailable = false;
+    this.filteredViaLocation = result;
+    this.getList(result);
+  }
+
+  applyFilter(filterOpts) {
+    if (filterOpts.reset) {
+      this.filteredAds = this.properties;
+      return;
     }
+    if (this.filteredViaLocation) {
+      this.filteredAds = this.filteredViaLocation;
+    } else this.filteredAds = this.properties;
+
+    this.filteredAds = this.filterService.applyFilter(
+      this.filteredAds,
+      filterOpts
+    );
+    this.getList(this.filteredAds);
   }
 }
