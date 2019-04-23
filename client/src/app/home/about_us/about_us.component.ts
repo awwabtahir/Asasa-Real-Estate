a71c16e0f92342e16463f8b6333f803849c4fcd8
Danish7520 <danihslashari3@gmail.com>
2019-04-23 15:45:51 +0500

    About us added

---
+++ client/src/app/home/about_us/about_us.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "aboutus",
+  templateUrl: "./about_us.component.html",
+  styleUrls: ["./about_us.component.css"]
+})
+export class AboutUsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
