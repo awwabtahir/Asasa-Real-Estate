1df8b51089957393058eb7533e367b0329a6b2d7
Danish7520 <danihslashari3@gmail.com>
2019-04-29 16:34:59 +0500

    Marker for Asasa location done in contact us page

--- client/src/app/home/contact_us/contact_us.component.ts
+++ client/src/app/home/contact_us/contact_us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { MapService } from "shared/services/map.service";
+import { AuthenticationService } from "app/authentication.service";
 
 @Component({
   selector: "contactus",
@@ -6,7 +8,37 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./contact_us.component.css"]
 })
 export class ContactUsComponent implements OnInit {
+  // labelOptions = {
+  //   color: "black",
+  //   fontFamily: "",
+  //   fontSize: "20px",
+  //   fontWeight: "bold",
+  //   text: "ASASA Real Estate"
+  // };
+  icon = {
+    url: "../../../assets/images/House.png",
+    scaledSize: {
+      width: 60,
+      height: 80
+    }
+  };
+  title: string = "AGM project";
+  latitude: number;
+  longitude: number;
+  zoom: number;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setCurrentLocation();
+  }
+  setCurrentLocation() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.latitude = 33.703422;
+        this.longitude = 72.979708;
+        this.zoom = 17;
+      });
+    }
+  }
 }
