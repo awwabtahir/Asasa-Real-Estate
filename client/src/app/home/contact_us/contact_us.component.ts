60de5620ca3c96b08de19d110ed2e80f57181a1f
Danish7520 <danihslashari3@gmail.com>
2019-04-30 15:47:31 +0500

    Contact us page updated

--- client/src/app/home/contact_us/contact_us.component.ts
+++ client/src/app/home/contact_us/contact_us.component.ts
@@ -16,18 +16,25 @@ export class ContactUsComponent implements OnInit {
   //   text: "ASASA Real Estate"
   // };
   icon = {
-    url: "../../../assets/images/House.png",
+    url: "assets/images/map_pin.png",
     scaledSize: {
-      width: 60,
-      height: 80
+      width: 180,
+      height: 90
     }
   };
   title: string = "AGM project";
   latitude: number;
   longitude: number;
   zoom: number;
+  @Input() id;
 
-  constructor() {}
+  name = "";
+  email = "";
+  phone = "";
+  type = "";
+  optin = true;
+
+  constructor(private auth: AuthenticationService) {}
 
   ngOnInit() {
     this.setCurrentLocation();
@@ -35,9 +42,10 @@ export class ContactUsComponent implements OnInit {
   setCurrentLocation() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
-        this.latitude = 33.703422;
-        this.longitude = 72.979708;
+        this.latitude = 33.70357;
+        this.longitude = 72.979746;
         this.zoom = 17;
+        // 33.703570, 72.979746
       });
     }
   }
